60ce2e23a448dc52647f4bcc789f108f7f15234c
Adrienn Csontak <adrienn@yyt.dev>
2023-12-10 23:19:34 +0000

    day05 part 2

--- day05/src/main.rs
+++ day05/src/main.rs
@@ -1,9 +1,9 @@
-use std::fmt::Alignment;
-
 fn main() {
     let input = include_str!("input.txt");
     let res_part_1 = part_1(input);
     println!("{}", res_part_1);
+    let res_part_2 = part_2(input);
+    println!("{}", res_part_2);
 }
 
 struct RangeMapping {
@@ -13,7 +13,7 @@ struct RangeMapping {
 }
 
 impl RangeMapping {
-    fn get_source(&self, num: &i64) -> Option<i64> {
+    fn get_destination(&self, num: &i64) -> Option<i64> {
         if let Some(diff) =
             (num.checked_sub(self.source_start)).filter(|&n| n >= 0 && n < self.range)
         {
@@ -29,6 +29,63 @@ struct Almanac {
     maps: Vec<Vec<RangeMapping>>,
 }
 
+fn get_destinations(mappings: &Vec<RangeMapping>, num_ranges: Vec<Vec<i64>>) -> Vec<Vec<i64>> {
+    let mut destinations: Vec<Vec<i64>> = Vec::new();
+    let mut current_ranges: Vec<Vec<i64>> = Vec::from(num_ranges);
+
+    loop {
+        let mut remainders: Vec<Vec<i64>> = Vec::new();
+        for r in current_ranges {
+            let start = r[0];
+            let range = r[1];
+            let end = start + range - 1;
+            let mut has_overlap: bool = false;
+
+            for m in mappings {
+                let m_end = m.source_start + m.range - 1;
+                if is_overlapping(start, end, m.source_start, m_end) {
+                    has_overlap = true;
+                    let dest_start: i64;
+                    let dest_end: i64;
+                    let diff = m.destination_start - m.source_start;
+
+                    if m.source_start <= start {
+                        dest_start = start + diff;
+                    } else {
+                        dest_start = m.destination_start;
+                        remainders.push(vec![start, m.source_start - 1 - start]);
+                    }
+                    if end <= m_end {
+                        dest_end = end + diff;
+                    } else {
+                        dest_end = m.destination_start + m.range - 1;
+                        remainders.push(vec![m_end + 1, end - m_end + 1]);
+                    }
+
+                    destinations.push(vec![dest_start, dest_end - dest_start + 1]);
+                } else {
+                    if has_overlap {
+                        break;
+                    }
+                }
+            }
+
+            if !has_overlap && remainders.is_empty() {
+                destinations.push(vec![start, range]);
+            }
+        }
+        if remainders.is_empty() {
+            break;
+        }
+        current_ranges = remainders;
+    }
+    destinations
+}
+
+fn is_overlapping(start_a: i64, end_a: i64, start_b: i64, end_b: i64) -> bool {
+    start_a.max(start_b) <= end_a.min(end_b)
+}
+
 fn part_1(input: &str) -> i64 {
     let almanac = parse_input(input);
     almanac
@@ -39,7 +96,7 @@ fn part_1(input: &str) -> i64 {
                 .map(|num| {
                     mappings
                         .iter()
-                        .find_map(|m| m.get_source(num))
+                        .find_map(|m| m.get_destination(num))
                         .unwrap_or(*num)
                 })
                 .collect()
@@ -60,9 +117,8 @@ fn parse_input(input: &str) -> Almanac {
         .collect();
 
     let maps = blocks
-        .skip(1)
         .map(|block| {
-            block
+            let mut mapping = block
                 .lines()
                 .skip(1)
                 .map(|mapping| {
@@ -75,15 +131,33 @@ fn parse_input(input: &str) -> Almanac {
                         range: nums.next().unwrap(),
                     }
                 })
-                .collect::<Vec<RangeMapping>>()
+                .collect::<Vec<RangeMapping>>();
+            mapping.sort_by_key(|mapping| mapping.source_start);
+            mapping
         })
         .collect();
+
     Almanac { seeds, maps }
+}
+
+fn part_2(input: &str) -> i64 {
+    let almanac = parse_input(input);
+    let seeds: Vec<Vec<i64>> = almanac
+        .seeds
+        .chunks(2)
+        .map(|chunk| chunk.to_vec())
+        .collect();
+
+    let mut current_ranges = Vec::from(seeds);
+    for mapping in almanac.maps {
+        current_ranges = get_destinations(&mapping, current_ranges.clone());
+    }
+    current_ranges.iter().map(|v| v[0]).min().unwrap()
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::part_1;
+    use crate::{part_1, part_2};
 
     #[test]
     fn calculates_part_1_correctly() {
@@ -122,6 +196,46 @@ humidity-to-location map:
 56 93 4";
         let actual = part_1(input);
         let expected = 35;
+        assert_eq!(actual, expected)
+    }
+
+    #[test]
+    fn calculates_part_2_correctly() {
+        let input = "seeds: 79 14 55 13
+
+seed-to-soil map:
+50 98 2
+52 50 48
+
+soil-to-fertilizer map:
+0 15 37
+37 52 2
+39 0 15
+
+fertilizer-to-water map:
+49 53 8
+0 11 42
+42 0 7
+57 7 4
+
+water-to-light map:
+88 18 7
+18 25 70
+
+light-to-temperature map:
+45 77 23
+81 45 19
+68 64 13
+
+temperature-to-humidity map:
+0 69 1
+1 0 69
+
+humidity-to-location map:
+60 56 37
+56 93 4";
+        let actual = part_2(input);
+        let expected = 46;
         assert_eq!(actual, expected)
     }
 }
